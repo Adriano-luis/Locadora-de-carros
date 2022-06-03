aea92c66a63ed28707d7cb4ee7106faabcc089ee
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-06-03 17:25:44 -0300

    AbstractRepository done

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -1,35 +1,32 @@
 <?php
 
 namespace App\Repositories;
+// use Illuminate\Database\Eloquent\Model;
+class MarcaRepository extends AbstractRepository{
 
+    // public function __construct(Model $model){
+    //     $this->model = $model;
+    // }
 
-use Illuminate\Database\Eloquent\Model;
+    // public function selectAttibutosRegristrosRelacionados($atributos){
+    //     $this->model = $this->model->with($atributos);
+    // }
 
-class MarcaRepository {
+    // public function filtro($filtros){
+    //     $filtros = explode(';',$filtros);
+    //     foreach($filtros as $condicao){
+    //         $c = explode(':',$condicao);
+    //         $this->model = $this->model->where($c[0],$c[1],$c[2]);
+    //     }
+    // }
 
-    public function __construct(Model $model){
-        $this->model = $model;
-    }
+    // public function selectAtributos($atributos){
+    //     $this->model = $this->model->selectRaw($atributos);
+    // }
 
-    public function selectAttibutosRegristrosRelacionados($atributos){
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro($filtros){
-        $filtros = explode(';',$filtros);
-        foreach($filtros as $condicao){
-            $c = explode(':',$condicao);
-            $this->model = $this->model->where($c[0],$c[1],$c[2]);
-        }
-    }
-
-    public function selectAtributos($atributos){
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado(){
-        return $this->model->get();
-    }
+    // public function getResultado(){
+    //     return $this->model->get();
+    // }
 }
 
 
