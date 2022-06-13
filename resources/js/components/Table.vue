f42caed5a3048e512cd7b0dac2d90678630aa987
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-06-13 01:04:23 -0300

    avoiding dependency in component table

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -2,14 +2,25 @@
     <table class="table table-hover">
         <thead>
             <tr>
-                <th scope="col" v-for="t, key in titulos" :key="key">{{t}}</th>
+                <th scope="col" v-for="t, key in titulos" :key="key" class="text-uppercase">{{t}}</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="m in dados" :key="m.id">
-                <th scope="row">{{m.id}}</th>
+            <tr v-for="obj in dados" :key="obj.id">
+                <template v-for="(valor, chave) in obj">
+                    <td v-if="titulos.includes(chave)" :key="chave">
+                        <span v-if="chave == 'imagem'">
+                            <img :src="'/public/storage/'+valor" width="35" height="35">
+                        </span>
+                        <span v-else>
+                            {{valor}}
+                        </span>
+                    </td>
+                </template>
+
+                <!-- <th scope="row">{{m.id}}</th>
                 <td>{{m.nome}}</td>
-                <td><img :src="'/public/storage/'+m.imagem" :alt="'Logo '+m.nome" width="35" height="35"></td>
+                <td><img :src="'/public/storage/'+m.imagem" :alt="'Logo '+m.nome" width="35" height="35"></td> -->
             </tr>
         </tbody>
     </table>
